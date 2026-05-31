7358c5a1c33ae7cea012d14deed12b02fe9664e7
Howksick <yanzakharevsk@mail.ru>
2026-05-31 10:21:38 +0300

    Add userRole enum with two roles:ADMIN and USER

---
+++ src/main/java/com/innowise/auth_service/jpa/enums/UserRole.java
@@ -0,0 +1,14 @@
+package com.innowise.auth_service.jpa.enums;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+
+public enum UserRole implements GrantedAuthority {
+    ADMIN, USER;
+
+    @Override
+    public @Nullable String getAuthority() {
+
+        return "ROLE_" + name();
+    }
+}
