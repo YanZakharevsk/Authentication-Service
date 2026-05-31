a0fb240553ffac7f21f74a5805ac60ef3c8fcc88
Howksick <yanzakharevsk@mail.ru>
2026-05-31 10:31:07 +0300

    Add RefreshTOken entity for storage jwt-refresh-token

---
+++ src/main/java/com/innowise/auth_service/jpa/entity/RefreshToken.java
@@ -0,0 +1,64 @@
+package com.innowise.auth_service.jpa.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "refresh_tokens")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(nullable = false, unique = true, length = 1000)
+    private String token;
+
+    @Column(name = "expires_at", nullable = false)
+    private Instant expiresAt;
+
+    @Column(nullable = false)
+    private Boolean revoked = false;
+
+    @Override
+    public final boolean equals(Object o) {
+        if(this == o) return true;
+        if(o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy
+                ?((HibernateProxy) o).getHibernateLazyInitializer()
+                 .getPersistentClass()
+                : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy
+                ?((HibernateProxy) this).getHibernateLazyInitializer()
+                 .getPersistentClass()
+                : this.getClass();
+        if(thisEffectiveClass != oEffectiveClass) return false;
+        RefreshToken refreshToken = (RefreshToken) o;
+        return getId() != null && Objects.equals(getId(), refreshToken.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return this instanceof HibernateProxy
+                ? ((HibernateProxy) this).getHibernateLazyInitializer()
+                  .getPersistentClass()
+                  .hashCode()
+                : getClass().hashCode();
+    }
+
+
+}
