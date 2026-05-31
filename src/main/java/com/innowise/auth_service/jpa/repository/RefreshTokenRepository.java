6eb20f5374f05f3cbe1c55d5d3e4e22d9496ad81
Howksick <yanzakharevsk@mail.ru>
2026-05-31 10:53:16 +0300

    Add RefreshTokenRepository for main operations with RefreshToken(find, delete, exists)

---
+++ src/main/java/com/innowise/auth_service/jpa/repository/RefreshTokenRepository.java
@@ -0,0 +1,25 @@
+package com.innowise.auth_service.jpa.repository;
+
+import com.innowise.auth_service.jpa.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+    Optional<RefreshToken> findByToken(String token);
+
+    List<RefreshToken> findByUserId(Long userId);
+
+    List<RefreshToken> findByUserIdAndRevokedFalse(Long userId);
+
+    boolean deleteByUserId(Long userId);
+
+    boolean deleteByExpiresAtBefore(Instant expiresAtBefore);
+
+    boolean existsByToken(String token);
+}
